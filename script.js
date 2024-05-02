7de0b8dd037f6c8b1588a80f44043bdd19f4172e
Jordan Leonard <leonard_2010@live.com>
2024-05-01 21:52:02 -0500

    condensed getting location

--- script.js
+++ script.js
@@ -39,26 +39,7 @@ function startGame() {
     const player1Turn = () => {
       let choice = prompt(`${players.player1.title} pick a number to put your symbol 1-9`);
       let number = parseInt(choice);
-  
-      //get row of board
-      let row = 0;
-      if (number <= 3) {
-        row = 0;
-      }else if (number >= 4 && number <= 6) {
-        row = 1;
-      }else {
-        row = 2;
-      }
-  
-      //square of row
-      if (number <= 3) {
-        number = number - 1;
-      }else if (number >= 4 && number <= 6) {
-        number = number - 4;
-      }else {
-        number = number - 7;
-      }
-  
+      getLocation(number);
       gameboard.board[row][number] = players.player1.symbol;
       drawBoard(round, gameboard);
     }
@@ -66,29 +47,35 @@ function startGame() {
     const player2Turn = () => {
       let choice = prompt(`${players.player2.title} pick a number to put your symbol 1-9`);
       let number = parseInt(choice);
-  
-      //get row of board
-      let row = 0;
-      if (number <= 3) {
-        row = 0;
-      }else if (number >= 4 && number <= 6) {
-        row = 1;
-      }else {
-        row = 2;
-      }
-  
-      //square of row
-      if (number <= 3) {
-        number = number - 1;
-      }else if (number >= 4 && number <= 6) {
-        number = number - 4;
-      }else {
-        number = number - 7;
-      }
-  
+      getLocation(number);
       gameboard.board[row][number] = players.player2.symbol;
       drawBoard(round, gameboard);
     }
   }
 }
+
+// could also make get row and square a function to condense 
+function getLocation(number) {
+        //get row of board
+        let row = 0;
+        if (number <= 3) {
+          row = 0;
+        }else if (number >= 4 && number <= 6) {
+          row = 1;
+        }else {
+          row = 2;
+        }
+    
+        //square of row
+        if (number <= 3) {
+          number = number - 1;
+        }else if (number >= 4 && number <= 6) {
+          number = number - 4;
+        }else {
+          number = number - 7;
+        }
+        return row, number;
+}
+//function checkForSymbol (gameboard.board[row][number]) if a symbol is there alert spot taken and call players turn to start again
+
 //still need check square for player symbol and reject if one there as well as winning conditions
