9bdd5dd5b24a0a2d0dc67c6d885f9fd1525ed836
Jordan Leonard <leonard_2010@live.com>
2024-05-02 21:30:24 -0500

    turn tracker made

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Type `startGame()` to start");
-function drawBoard(round, gameboard) {
-  console.group(`Score after round ${round}`);
+function drawBoard(turnCounter, gameboard) {
+  console.group(`Score after round ${turnCounter}`);
   console.log("");
-  console.log((gameboard.board[0][0] || '1') + " | " + (gameboard.board[0][1] || ' 2 ') + " | " + (gameboard.board[0][2] || ' 3 '));
+  console.log((gameboard.board[0][0] || ' 1 ') + " | " + (gameboard.board[0][1] || ' 2 ') + " | " + (gameboard.board[0][2] || ' 3 '));
   console.log("--------------");
-  console.log((gameboard.board[1][0] || '4') + " | " + (gameboard.board[1][1] || ' 5 ') + " | " + (gameboard.board[1][2] || ' 6 '));
+  console.log((gameboard.board[1][0] || ' 4 ') + " | " + (gameboard.board[1][1] || ' 5 ') + " | " + (gameboard.board[1][2] || ' 6 '));
   console.log("--------------");
-  console.log((gameboard.board[2][0] || '7') + " | " + (gameboard.board[2][1] || ' 8 ') + " | " + (gameboard.board[2][2] || ' 9 '));
+  console.log((gameboard.board[2][0] || ' 7 ') + " | " + (gameboard.board[2][1] || ' 8 ') + " | " + (gameboard.board[2][2] || ' 9 '));
   console.groundEnd;
 }
 
@@ -31,51 +31,74 @@ function startGame() {
       [null, null, null]
     ]
   }
-  drawBoard(1, gameboard);
+  drawBoard(0, gameboard);
 
   //playthrough logic
-  for(let i = 1; i <= 9; i++) {
-    let round = i;
-    const player1Turn = () => {
+  let turnCounter = 1;
+  while(turnCounter <= 9) {
+    if (turnCounter % 2 !== 0) {
       let choice = prompt(`${players.player1.title} pick a number to put your symbol 1-9`);
       let number = parseInt(choice);
-      getLocation(number);
-      gameboard.board[row][number] = players.player1.symbol;
-      drawBoard(round, gameboard);
-    }
+      let row = 0;
 
-    const player2Turn = () => {
+      if (number <= 3) {
+        row = 0;
+      }else if (number >= 4 && number <= 6) {
+        row = 1;
+      }else {
+        row = 2;
+      }
+        
+      //square of row
+      if (number <= 3) {
+        number = number - 1;
+      }else if (number >= 4 && number <= 6) {
+        number = number - 4;
+      }else {
+        number = number - 7;
+      }
+      
+      gameboard.board[row][number] = players.player1.symbol;
+      drawBoard(turnCounter, gameboard);
+      turnCounter++;
+
+    }else if (turnCounter % 2 === 0) {
       let choice = prompt(`${players.player2.title} pick a number to put your symbol 1-9`);
       let number = parseInt(choice);
-      getLocation(number);
+      let row = 0;
+      if (number <= 3) {
+        row = 0;
+      }else if (number >= 4 && number <= 6) {
+        row = 1;
+      }else {
+        row = 2;
+      }
+        
+      //square of row
+      if (number <= 3) {
+        number = number - 1;
+      }else if (number >= 4 && number <= 6) {
+        number = number - 4;
+      }else {
+        number = number - 7;
+      }
       gameboard.board[row][number] = players.player2.symbol;
-      drawBoard(round, gameboard);
+      drawBoard(turnCounter, gameboard);
+      turnCounter++;
+    }
+  }
+  solutions();
+  function solutions() {
+    let p1Symbol = players.player1.symbol;
+    let p2Symbol = players.player2.symbol;
+    if (gameboard.board[0][0] && gameboard.board[0][3] && gameboard.board[0][6] === p1Symbol){
+      console.log("Player 1 wins this round");
+    }else{
+      console.log("Draw try again");
     }
   }
 }
 
-// could also make get row and square a function to condense 
-function getLocation(number) {
-        //get row of board
-        let row = 0;
-        if (number <= 3) {
-          row = 0;
-        }else if (number >= 4 && number <= 6) {
-          row = 1;
-        }else {
-          row = 2;
-        }
-    
-        //square of row
-        if (number <= 3) {
-          number = number - 1;
-        }else if (number >= 4 && number <= 6) {
-          number = number - 4;
-        }else {
-          number = number - 7;
-        }
-        return row, number;
-}
 //function checkForSymbol (gameboard.board[row][number]) if a symbol is there alert spot taken and call players turn to start again
 
 //still need check square for player symbol and reject if one there as well as winning conditions
